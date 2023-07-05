a9ac48c6053fe7ce61864c0b08b6663bd81dcdef
Siraphop Sukchu <86290693+FujaTyping@users.noreply.github.com>
2023-07-05 23:43:59 +0700

    Add express web service

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const { SapphireClient } = require('@sapphire/framework');
 const { GatewayIntentBits } = require('discord.js');
 const { prefix } = require('./config.json');
 
-require('dotenv').config()
+require('dotenv').config() // Use .env file
 const discord_token = process.env.discord_token
+
+const express = require('express') // Host web service
+const app = express()
 
 const client = new SapphireClient({
     defaultPrefix: prefix, // Custom prefix
@@ -17,6 +20,12 @@ const main = async () => {
         client.logger.info('Masayuki is waking up...');
         await client.login(discord_token);
         client.logger.info('Masayuki is wake up!');
+        // Send a simple message
+        app.get('/', function (req, res) {
+            res.send('Hi - From Masayuki')
+        })
+        app.listen(1317)
+        client.logger.info('Web service is ready!');
     } catch (error) {
         client.logger.fatal(error);
         client.destroy();
