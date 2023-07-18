5bbef6ea63f05f723bb24eb66a46f70eb8520dda
Siraphop Sukchu <86290693+FujaTyping@users.noreply.github.com>
2023-07-18 22:55:17 +0700

    Add more information for Discord command

--- src/commands/Info/lookup.js
+++ src/commands/Info/lookup.js
@@ -23,14 +23,18 @@ class DiscordCommand extends Command {
 
         const Server = interaction.guild
         const User = interaction.options.getUser('user')
+        const Member = Server.members.cache.get(User.id);
+        const Permissions = Member.permissions.toArray();
+        const PermissionsString = Permissions.join(' , ');
 
         const Content = new EmbedBuilder()
             .setColor(14425658)
             .setTitle(`👤 Lookup for ${User.username}`)
-            .setDescription(`- 📃 Server : ${Server.name}\n- 🧩 Discriminator : #${User.discriminator}\n- 🆔 Id : ${User.id}\n- 🤖 Bot : ${User.bot}\n- 🔧 System : ${User.system}\n- 🔨 Created at : ${User.createdAt.toLocaleString()}`)
+            .setDescription(`- 🖋 Nickname : ${Member.nickname}\n- 📃 Server : ${Server.name}\n- 🌎 Locale : ${Server.preferredLocale}\n- 🧩 Discriminator : #${User.discriminator}\n- 🆔 Id : ${User.id}\n- 🤖 Bot : ${User.bot}\n- 🔧 System : ${User.system}\n- 🔨 Created at : ${User.createdAt.toLocaleString()}`)
             .addFields(
                 { name: 'ฺ🖼 Banner', value: `${User.banner}`, inline: true },
                 { name: '🎨 Accent color', value: `${User.accentColor}`, inline: true },
+                { name: '🔐 Permissions', value: `${PermissionsString}` },
             )
             .setThumbnail(`${User.avatarURL()}`)
             .setTimestamp()
