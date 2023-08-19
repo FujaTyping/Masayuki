f453556a9a758b5585d69ac2704b8484c7113015
DaFujaTyping <142676101+DaFujaTyping@users.noreply.github.com>
2023-08-19 22:50:47 +0700

    Update talk warn messagae

--- src/commands/Miscellaneous/talk.js
+++ src/commands/Miscellaneous/talk.js
@@ -18,7 +18,7 @@ class TalkCommand extends Command {
         const Warn = new EmbedBuilder()
             .setColor(14425658)
             .setTitle("💥 Feature unavailable")
-            .setDescription(`⚠ Sorry ` + `${Author}` + ", Masayuki `!!talk (tk)` feature is unavailable right now!\nThis command require `DELETE_MESSAGE (MANAGE_MESSAGE)` permission\nNow we're fixing the Masayuki default permission")
+            .setDescription(`⚠ Sorry ` + `${Author}` + ", Masayuki `!!talk` feature is unavailable right now!\nThis feature require `DELETE_MESSAGE (MANAGE_MESSAGE)` permission\nNow we're fixing : Default permission")
             .setTimestamp()
 
         const msg = await message.channel.send({ embeds: [Warn] });
